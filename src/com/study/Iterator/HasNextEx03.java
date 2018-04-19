227472a212e9bb142ee1805f4efb997d69a847fb
mingooddev <mingood92@gmail.com>
2018-04-19 20:39:52 +0900

    Update Iterator

---
+++ src/com/study/Iterator/HasNextEx03.java
@@ -0,0 +1,33 @@
+package com.study.Iterator;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+public class HasNextEx03 {
+
+    public static void main(String[] args) {
+
+        Collection<Integer> c = new ArrayList<Integer>();
+
+        for (int i = 0; i < 10; ++i) {
+            c.add(i);
+            c.add(i);
+        }
+
+        for (int i : c) System.out.print(i + " ");
+
+        System.out.println();
+
+        Iterator<Integer> iterator = c.iterator();
+        while (iterator.hasNext()) {
+            int i = iterator.next();
+            if (i % 2 == 0)
+                iterator.remove();
+        }
+
+        for (int i : c)
+            System.out.print(i + " ");
+    }
+
+}
