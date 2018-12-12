a3d2ca1dbde098f9a3372d209c86223924b85f21
MinGOODdev <mingood92@gmail.com>
2018-12-12 21:17:34 +0900

    Feat: Java8 TimeAPI - LocalTime

---
+++ src/java8/time/LocalTimeTest.java
@@ -0,0 +1,36 @@
+package java8.time;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.time.LocalTime;
+
+public class LocalTimeTest {
+
+  private static final LocalTime nowTime = LocalTime.now();
+
+  @BeforeClass
+  public static void beforeClass() {
+    System.out.println("Now : " + nowTime);
+    System.out.println("Min : " + LocalTime.MIN);
+    System.out.println("Max : " + LocalTime.MAX);
+    System.out.println("Noon : " + LocalTime.NOON);
+    System.out.println("Mid Night : " + LocalTime.MIDNIGHT);
+    System.out.println();
+
+    System.out.println("Now Hour : " + nowTime.getHour());
+    System.out.println("Now Minute : " + nowTime.getMinute());
+    System.out.println("Now Second : " + nowTime.getSecond());
+    System.out.println();
+
+    System.out.println("20:00 : " + LocalTime.of(20, 0 ,0));
+    System.out.println("After 1 Hours : " + nowTime.plusHours(1));
+  }
+
+  @Test
+  public void test() {
+    Assert.assertEquals(nowTime.getHour(), 10);
+  }
+
+}
